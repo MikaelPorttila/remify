0ff7e88a4dfe047bb76f82029211e725752a8949
Mikael Porttila <mikael.porttila@gmail.com>
2022-11-19 15:05:17 +0100

    remove more comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { replaceAll } from './helpers'; 
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('remify.remify', () => {
 		const editor = vscode.window.activeTextEditor;
 		let selectedText = editor?.document.getText(editor.selection) as string;
