97589266786cd8a7e7710d3a541f03d49c5ba05e
Mikael Porttila <mikael.porttila@gmail.com>
2022-11-20 10:21:40 +0100

    minor refactor

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ A lightweight and easy-to-use extension for converting CSS units to REM units
 
 ## Features
 
-- PX -> REM Conversion
+- PX -> REM
+- PT -> REM
+- IN -> REM
+- MM -> REM
+- CM -> REM
+- PC -> REM
 
 
 ## Local install
--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,28 @@ import * as vscode from 'vscode';
 import { replaceAll } from './helpers'; 
 
 export function activate(context: vscode.ExtensionContext) {
+	const supportedUnits = new Map([
+		['px', { multiplier: 0.0625 }],
+		['pt', { multiplier: 0.0833 }],
+		['in', { multiplier: 6 }],
+		['mm', { multiplier: 0.2362 }],
+		['cm', { multiplier: 2.3622 }],
+		['pc', { multiplier: 7.2890 }]
+	]);
+
 	let disposable = vscode.commands.registerCommand('remify.remify', () => {
 		const editor = vscode.window.activeTextEditor;
 		let selectedText = editor?.document.getText(editor.selection) as string;
 		
 		if (!selectedText) {
+			vscode.window.showInformationMessage('Select text containing non-rem units.');
 			return;
 		}
 
 		const initialText = selectedText;
 
 		let replacements: any = [];
-		const matches = selectedText.matchAll(/\d*\.?\d+(?:(px)|%)/g);
+		const matches = selectedText.matchAll(/\d*\.?\d+(?:(px|pt|in|mm|cm|pc)|%)/g);
 		let entriesCounter = 0;
 		let batchCallCounter = 0;
 
@@ -24,24 +34,15 @@ export function activate(context: vscode.ExtensionContext) {
 			if (!replacements[text]) {
 				let value = parseInt(text, 10);
 				const unit = text.split(value + '')[1].toLocaleLowerCase();
+				const unitFormat = supportedUnits.get(unit);
 
-				if (unit) {
-					let supported = false;
-					switch(unit) {
-						case 'px':
-							value = value * 0.0625;
-							supported = true;
-						break;
-						default:
-							console.log('Unsupported unit', unit);
-						break;
-					}
-
-					if (supported) {
-						replacements[text] = true;
-						selectedText = replaceAll(selectedText, text, value + 'rem');
-						batchCallCounter++;
-					}
+				if (unitFormat) {
+					value = value * unitFormat.multiplier;
+					replacements[text] = true;
+					selectedText = replaceAll(selectedText, text, value + 'rem');
+					batchCallCounter++;
+				} else {
+					console.log('Unsupported unit', unit);
 				}
 			}
 		}
